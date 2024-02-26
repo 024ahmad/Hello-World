2d76a556617e51d31323a394a8853c820004d19a
Ahmad Raza <159168988+024ahmad@users.noreply.github.com>
2024-02-26 15:40:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Simple Mehod of Print Hello World
+console.log ("Hello World Assalam u alikum");
+// Using Variable
+var a = "Hello World";
+console.log(a);
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Simple Mehod of Print Hello World
+
+console.log ("Hello World");
+
+// Using Variable
+
+let a:string = "Hello World";
+
+console.log (a)
